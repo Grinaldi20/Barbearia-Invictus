e8adc68ca5a5247f8fe717dfd8c957f6755d99ed
Murilo Pereira Grinaldi <160647039+Grinaldi20@users.noreply.github.com>
2026-05-22 20:42:52 -0300

    rotas Admin

--- Site-Barbearia/src/main.jsx
+++ Site-Barbearia/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin" element={<admin />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
